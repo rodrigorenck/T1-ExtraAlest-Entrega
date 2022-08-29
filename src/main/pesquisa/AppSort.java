c4dab87683d6cac86f342ba3885abf947aeb1492
Leonardo Heredia <leonardo.heredia@hp.com>
2022-08-29 09:49:24 -0300

    Pesquisa

--- src/main/pesquisa/AppSort.java
+++ src/main/pesquisa/AppSort.java
@@ -4,18 +4,17 @@ public class AppSort {
 
     public static void main(String args[]) {
         Random r = new Random();
-        for (int max = 10; max < 100000; max += 500) {
+        long start_start = System.nanoTime();
+        for (int max = 10; max <= 300_000; max += 500) {
             int[] data = new int[max];
             for (int i = 0; i < data.length; i++)
                 data[i] = r.nextInt(data.length * 10);
 
             long start = System.nanoTime();
             Bubblesort s = new Bubblesort();
-            // Mergesort s = new Mergesort();
-            // Quicksort s = new Quicksort();
-            // s.sort(data);
-            //MaxHeap my = new MaxHeap(data);
-            //my.sort();
+            //Mergesort s = new Mergesort();
+            //Quicksort s = new Quicksort();
+            s.sort(data);
             long end = System.nanoTime();
             // for(int i=0; i<data.length; i++)
             // System.out.print(data[i]+" ");
@@ -23,5 +22,9 @@ public class AppSort {
             double tempo = (end - start) / 1e6; // 1.000.000 (para transformar em ms)
             System.out.printf("%d %.2f\n", max, tempo);
         }
+        long end_end = System.nanoTime();
+        double tempo = (end_end - start_start) / 1e6; // 1.000.000 (para transformar em ms)
+        System.out.printf("Tempo total: %.2f\n", tempo);
+
     }
 }
